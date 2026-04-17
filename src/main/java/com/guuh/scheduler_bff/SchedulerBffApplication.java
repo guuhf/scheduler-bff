dec5b69ac5b69f3a88246584e016fe4ca21d4e57
guuh <gustavo.jacintof@gmail.com>
2026-04-17 15:58:10 -0300

    initial commit

---
+++ src/main/java/com/guuh/scheduler_bff/SchedulerBffApplication.java
@@ -0,0 +1,13 @@
+package com.guuh.scheduler_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulerBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guuh/scheduler_bff/SchedulerBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guuh.scheduler_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
